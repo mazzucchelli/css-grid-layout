64c3164bf388c125fe001db0f0adc4e958485401
mmazzucchelli <mazzucchelli.matteo.91@gmail.com>
2018-10-13 18:01:08 +0200

    move notes on readme and fix html gulp task

--- node_tasks/html.js
+++ node_tasks/html.js
@@ -5,7 +5,7 @@ const $ = require('gulp-load-plugins')();
 var compileHtml = {
     compileHtml: function () {
         // TODO: Remove dashboard.html from gulp.src
-        return gulp.src(configs.paths.dev.base + '**/*.{html,njk}')
+        return gulp.src(configs.paths.dev.base + '*.{html,njk}')
             // .pipe($.data(() => ()))
             .pipe($.nunjucks.compile())
             .pipe(gulp.dest(configs.paths.dest.base));
